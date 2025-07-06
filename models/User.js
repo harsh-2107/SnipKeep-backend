120db06faea8fb8e2d553edbf94dfc6a8cef1215
Harsh Jaiswal <harsh.jaiswal2107@gmail.com>
2025-07-07 00:26:59 +0530

    chore: update mongoose model name

--- models/User.js
+++ models/User.js
@@ -1,10 +1,23 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    date: { type: Date, default: Date.now() }
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+    password: { 
+        type: String, 
+        required: true 
+    },
+    date: { 
+        type: Date, 
+        default: Date.now() 
+    }
 });
 
-export const User = mongoose.model('user', userSchema);
+export const User = mongoose.model('User', userSchema);
